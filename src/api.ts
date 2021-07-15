07b439d13a4aa45fad2d8f8e7c6897fae085e1ce
Eric <uhericc@gmail.com>
2021-07-15 00:02:46 -0700

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import * as Eris from "eris";
 
 export interface Cmd {
-    onHelp(): string;
+    onDesc(): string;
     onCommand(command: string): boolean;
     onRun(args: string[], msg: Eris.Message, client: Eris.Client): Promise<void>;
 }
